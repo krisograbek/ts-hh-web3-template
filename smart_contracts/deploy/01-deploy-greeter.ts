412cf3d2c844376645cf37779180bff75b8a242d
krisograbek <krzysztof.ograbek@gmail.com>
2022-04-29 09:48:38 +0200

    deploy greeter using npx hardhat deploy

---
+++ smart_contracts/deploy/01-deploy-greeter.ts
@@ -0,0 +1,19 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/dist/types'
+
+
+const deployGovernanceToken: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  // @ts-ignore
+  const { getNamedAccounts, deployments, network } = hre;
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  log("Deploying governance token by ", deployer)
+  const greeter = await deploy("Greeter", { // contract name
+    from: deployer,
+    args: ["Initial Hello!"],
+    log: true
+  })
+  log(`Deployed govenrance token to ${greeter.address}`)
+}
+
+export default deployGovernanceToken;
